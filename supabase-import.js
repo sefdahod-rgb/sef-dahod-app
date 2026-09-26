71f473c723b2061224049aaea5f896e3e7360363
Shubhi Education Foundation <sefdahod@gmail.com>
2026-09-26 13:18:39 +0530

    Delete supabase-import.js

--- supabase-import.js
+++
@@ -1,5 +0,0 @@
-
-// Import this in Supabase SQL Editor to load 79 students
-// Use students_data.json
-const students = require('./students_data.json');
-// Upload via Supabase Dashboard -> Table Editor -> Import
